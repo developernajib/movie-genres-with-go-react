f64b6dc744f6d09cc68a6108f893616a0385e105
developernajib <developernajib@gmail.com>
2025-07-06 12:02:01 +0600

    Docker setup & DB connect (messy)

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +12,8 @@ const port = 8080
 
 type application struct {
 	Domain string
+	DSN    string
+	DB     *sql.DB
 }
 
 func main() {
@@ -17,15 +21,22 @@ func main() {
 	var app application
 
 	// TODO: Read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
 
 	// TODO: Connect to database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
 
 	app.Domain = fmt.Sprintf("http://localhost:%d", port)
 
-	log.Println("Listening on port", port)
+	log.Println("App running on:", app.Domain)
 
 	// TODO: Start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
