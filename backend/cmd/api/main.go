e6e837022ffe49ae2e9752032878cab1a12e4662
developernajib <developernajib@gmail.com>
2025-07-06 18:28:59 +0600

    auth api done

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -7,25 +7,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 const port = 8080
 
 type application struct {
-	Domain string
-	DSN    string
-	DB     repository.DatabaseRepo
+	Domain       string
+	DSN          string
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
 func main() {
-	// TODO: Set application config
 	var app application
 
-	// TODO: Read from command line
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "somesupersecretkey", "Secret for JWT")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "Issuer for JWT")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "Audience for JWT")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "Domain for cookies")
+	flag.StringVar(&app.Domain, "domain", "example.com", "Domain for server")
 	flag.Parse()
 
-	// TODO: Connect to database
 	conn, err := app.connectToDB()
 	if err != nil {
 		log.Fatal(err)
@@ -34,11 +42,19 @@ func main() {
 	// defer conn.Close() // Closing the database connection also can be done this way
 	defer app.DB.Connection().Close()
 
-	app.Domain = fmt.Sprintf("http://localhost:%d", port)
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Hour * 1,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
+	}
 
 	log.Println("App running on:", app.Domain)
 
-	// TODO: Start a web server
 	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
